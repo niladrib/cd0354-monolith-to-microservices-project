8bdce98bd3e00dddc8a45eef408e0bba9f40a906
niladrib <niladri.bora@gmail.com>
2024-01-30 15:34:47 -0800

    Fixed connection string

--- udagram-api/src/sequelize.ts
+++ udagram-api/src/sequelize.ts
@@ -1,13 +1,22 @@
-import {Sequelize} from 'sequelize-typescript';
-import {config} from './config/config';
+import { Sequelize } from "sequelize-typescript";
+import { config } from "./config/config";
 
+console.log(
+  `params=${config.username} ${config.password} ${config.database} ${config.host} ${config.dialect}`
+);
 
 export const sequelize = new Sequelize({
-  'username': config.username,
-  'password': config.password,
-  'database': config.database,
-  'host': config.host,
+  username: config.username,
+  password: config.password,
+  database: config.database,
+  host: config.host,
 
-  'dialect': config.dialect,
-  'storage': ':memory:',
+  dialect: config.dialect,
+  storage: ":memory:",
+  dialectOptions: {
+    ssl: {
+      require: true, // This will help you. But you will see nwe error
+      rejectUnauthorized: false, // This line will fix new error
+    },
+  },
 });
